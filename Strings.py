7feaabd531dd7fdfaf1b12f781c823752cc16f69
Santhoshreddy-aiml <veerasantoshreddyreddem@gmail.com>
2025-01-07 06:01:50 +0000

    any name about project

---
+++ Strings.py
@@ -0,0 +1,4 @@
+str1 = "Santhosh"
+str2 = "reddy"
+result = str1 + "  @" + str2
+print(result)
---
+++ santhosh.py
@@ -0,0 +1,20 @@
+a=[1,2,'amazon','e-commerce','website']
+print(a)
+print(type(a))
+a.append("Zomato")
+print(a)
+a[3]
+print(a)
+a.insert(2,"hyd-west")
+print(a)
+#extend
+s=[1,2,3,4,5]
+s.extend([6,7])#add at the end of the list
+print(s)
+#sum of list
+print(sum(s))
+print(s*4)#add how many time in the list
+print(s.pop())#removes the last element
+print(s.pop(5))#remove the element in that particular place 
+print(s)
+print(s.count(5))#how many times the value is repeated
