bf0b51106a7090c98ff4af96ddbcb03976189982
Santhoshreddy-aiml <veerasantoshreddyreddem@gmail.com>
2025-01-07 06:18:17 +0000

    new

--- Strings.py
+++ Strings.py
@@ -2,3 +2,10 @@ str1 = "Santhosh"
 str2 = "reddy"
 result = str1 + "  @" + str2
 print(result)
+#dicitionary
+d = {1: 'Geeks', 2: 'For', 3: 'Geeks'}
+print(d)
+students_marks=[34,56,78,90,40]
+size=len(students_marks)
+print(size)
+#create a dicitionary with column names as key values in that add rows to predict marks of a students 
